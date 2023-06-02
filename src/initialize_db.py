2a07c67149d555c1018ea28548579216f07dbb47
vinicyusbrasil <vinicyus.brasil@baritecnologia.com.br>
2023-06-02 00:22:27 -0300

    fix: add log and error handling

--- src/initialize_db.py
+++ src/initialize_db.py
@@ -2,13 +2,13 @@ import json
 from typing import Tuple
 
 import pandas as pd
+import pymongo
 from bson import json_util
-from pymongo import MongoClient
 
 
 def connect_database():
 
-    client = MongoClient('mongodb://localhost:27017/')
+    client = pymongo.MongoClient('mongodb://localhost:27017/')
     cars_db = client.cars
 
     cars_collection = cars_db.Carros
@@ -23,7 +23,7 @@ def load_data() -> Tuple[pd.DataFrame, pd.DataFrame]:
 
     return dataframe_modelos, dataframe_montadoras
 
-def send_data(dataframe_modelos: pd.DataFrame, dataframe_montadoras: pd.DataFrame):
+def send_data(dataframe_modelos: pd.DataFrame, dataframe_montadoras: pd.DataFrame, cars_collection: pymongo.collection.Collection, manufacturers_collection: pymongo.collection.Collection):
     initial_cars = json.loads(dataframe_modelos.to_json(
         orient="records"))
 
@@ -74,18 +74,49 @@ def agregar(cars_collection):
 def parse_json(data):
     return json.loads(json_util.dumps(data))
 
+def main():
+
+    print('INFO: Starting ETL')
+    try:
+        cars_collection, manufacturers_collection = connect_database()
+    except Exception as e:
+        print(f"ERROR: {str(e)}")
+        raise
+
+    print("INFO: Connection successeful")
+    try:
+        dataframe_modelos, dataframe_montadoras = load_data()
+    except Exception as e:
+        print(f"ERROR: {str(e)}")
+        raise
+
+    print("INFO: CSVs loaded")
+
+    print("INFO: Updating data to the database")
+    try:
+        send_data(dataframe_modelos, dataframe_montadoras, cars_collection, manufacturers_collection)
+    except Exception as e:
+        print(f"ERROR: {str(e)}")
+        raise
+
+    try:
+        print("INFO: Transforming data ")
+        data = agregar(cars_collection)
+    except Exception as e:
+        print(f"ERROR: {str(e)}")
+        raise
+
+    print("INFO: Saving data")
+    try:
+        results_json = parse_json(data)
+        json.dump(results_json, open("data/payload.json", "w"))
+    except Exception as e:
+        print(f"ERROR: {str(e)}")
+        raise
+    print("INFO: Data succefully saved")
+
 
 if __name__ == "__main__":
+    main()
 
-    cars_collection, manufacturers_collection = connect_database()
-
-    dataframe_modelos, dataframe_montadoras = load_data()
-
-    send_data(dataframe_modelos, dataframe_montadoras)
-
-    data = agregar(cars_collection)
-
-    results_json = parse_json(data)
-
-    json.dump(results_json, open("data/payload.json", "w"))
 
